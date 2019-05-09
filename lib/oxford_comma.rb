f13e5549b79e680732f2da26d1b1d3d62eb17a85
hao wang <ge_nah@hotmail.com>
2019-05-09 20:26:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1 
+    "#{array[0]}"
+  elsif array.size == 2 
+    "#{array[0]} and #{array[1]}"
+  elsif
+    last_word = array.pop()
+    new_arr = array.join(", ")
+    "#{new_arr}, and #{last_word}"
+  end
 end
